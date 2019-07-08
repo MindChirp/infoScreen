ff244be267b43aeb0fd045f1bde9ba847b0152c9
MindChirp <33499052+MindChirp@users.noreply.github.com>
2019-07-08 19:43:02 +0200

    Updated boot.js

---
+++ boot.js
@@ -0,0 +1,22 @@
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+let win = null;
+let bootWin = null;
+
+function boot() {
+  //lage et nytt vindu
+    win = new BrowserWindow({
+    backgroundColor: "#171F26",
+    width: 1920,
+    height: 1080,
+    frame: false
+  }
+  )
+  //Laste inn html koden til vinduet
+  win.loadURL(url.format({
+    pathname: 'index.html',
+    slashes: true
+  }))
+}
+//Fyr av funksjon 'boot' når loading er ferdigstilt.
+app.on('ready', boot);
