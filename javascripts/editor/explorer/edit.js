5dc75304a3bf843e1514c542945e39983a2ec277
MindChirp <33499052+MindChirp@users.noreply.github.com>
2020-11-07 12:54:54 +0100

    Started on the edit page

---
+++ javascripts/editor/explorer/edit.js
@@ -0,0 +1,5 @@
+function updateEditPage(el) {
+    //All of the information about the slide is stored in the column element.
+    //Load all the data from the attribute tags, and display it in the edit page
+    
+}
